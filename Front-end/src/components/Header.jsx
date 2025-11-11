0d7381f1c96771e78703cb6e60850b23519d8fd8
Dan <uprise419@gmail.com>
2025-11-11 15:23:17 +0700

    memperbaiki front end

--- Front-end/src/components/Header.jsx
+++ Front-end/src/components/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
         <nav className="hidden md:flex items-center gap-8 text-sm font-medium text-gray-700">
           <a href="/program" className="hover:text-[#b38867]">Program</a>
           <a href="/Langganan" className="hover:text-[#b38867]">Langganan</a>
-          <a href="#" className="hover:text-[#b38867]">E-Learning</a>
+          <a href="/E-Learning" className="hover:text-[#b38867]">E-Learning</a>
         </nav>
 
         {/* === Kanan: Tombol Auth === */}
